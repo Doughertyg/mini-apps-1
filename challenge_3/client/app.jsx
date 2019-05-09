fbb5ad30e554223fed08d8791832250c203c77c2
Grahams Laptop <doughertyg37@gmail.com>
2019-05-09 12:22:44 -0700

    implemented challenge 3 to render each component as you click through

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -19,28 +19,26 @@ class App extends React.Component {
 		})
 	}
 
-	//separate each component out
-
 	render() {
 
 		return (
-			<div margin="auto">
-				<h1>Signup to Continue</h1>
-				<form>
-					<label>Name
-					<input placeholder="Type name here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'name')}} />
-					</label>
-					<label>Email
-					<input placeholder="type email here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} />
-					</label>
-					<label>password
-					<input type='password' onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} />
-					</label>
-					<input type="submit" value="next" />
-				</form>
+			<div id="myApp">
+				{this.state.page === 'home' ? <Home click={this.checkoutClick.bind(this)}/> : null}
+				{this.state.page === 'signup' ? <Signup /> : null}
 			</div>
 		)
-	}
+	}	
+}
+
+var Home = (props) => {
+	return (
+		<div margin="auto">
+			<h1>Grambos Goods</h1>
+			<img src="https://img.buzzfeed.com/buzzfeed-static/static/2014-05/enhanced/webdr07/2/6/enhanced-buzz-16707-1399026440-8.jpg" />
+			<h4>Banana Slicer From Amazon (only $29.99!)</h4>
+			<button onClick={props.click}>Checkout</button>				
+		</div>
+	)
 }
 
 class Signup extends React.Component {
@@ -64,19 +62,25 @@ class Signup extends React.Component {
 
 		return(
 			<div margin="auto">
-						<h1>Signup to Continue</h1>
-						<form>
-							<label>Name
-							<input placeholder="Type name here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'name')}} />
-							</label>
-							<label>Email
-							<input placeholder="type email here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} />
-							</label>
-							<label>password
-							<password onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} />
-							</label>
-							<input type="submit" value="next" />
-						</form>
+				<h1>Signup to Continue</h1>
+				<form>
+					<div>
+					<label>Name
+					<input placeholder="Type name here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'name')}} />
+					</label>
+					</div>
+					<div>
+						<label>Email
+						<input placeholder="type email here..." onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} />
+						</label>
+					</div>
+					<div>
+						<label>password
+						<input onChange={(e) => {(this.handleInput.bind(this))(e, 'email')}} type="password" />
+						</label>
+					</div>
+					<input type="submit" value="next" />
+				</form>
 			</div>
 		)
 	}
